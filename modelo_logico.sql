4290fa0da7c42fc5d94e84ee45640252e5a2089c
Itor Carlos <itor51carlos@gmail.com>
2024-08-25 20:22:54 -0300

    table Fornecedor

--- modelo_logico.sql
+++ modelo_logico.sql
@@ -18,3 +18,11 @@ CREATE TABLE Funcionario (
     data_admissao DATE,
     FOREIGN KEY (documento) REFERENCES Pessoa(documento)
 );
+
+CREATE TABLE Fornecedor(
+    documento VARCHAR(20) PRIMARY KEY,
+    nome nome,
+    telefones VARCHAR(20)[],
+	nome_fantasia VARCHAR(50),
+    FOREIGN KEY (documento) REFERENCES Pessoa(documento)
+);
